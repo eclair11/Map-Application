110c2f30315fbfac291be36cd0fc3dc5124bc381
Elias Romdan <elias.romdan@etu.univ-st-etienne.fr>
2020-12-10 19:01:14 +0100

    Add get hospitals by city feature

--- src/main/java/com/semweb/map/jena/Request.java
+++ src/main/java/com/semweb/map/jena/Request.java
@@ -15,7 +15,7 @@ import org.apache.jena.rdf.model.Model;
 public class Request {
 
     public static void main(String[] args) {
-        getCities();
+        getHospitalsByCity("Paris");
     }
 
     public static String getCities() {
@@ -23,8 +23,8 @@ public class Request {
         String endpoint = "http://localhost:3030/hospitals/sparql";
         String request = "PREFIX db: <http://dbpedia.org/ontology/> "
         + "PREFIX wd: <https://www.wikidata.org/wiki/> "
-        + "CONSTRUCT { wd:Q16917 db:city ?city } "
-        + "WHERE { ?hospital db:city ?city }";
+        + "CONSTRUCT { wd:Q16917 db:city ?city . } "
+        + "WHERE { ?hospital db:city ?city . }";
         Query query = QueryFactory.create(request, Syntax.syntaxARQ);
         QueryExecution exec = QueryExecutionFactory.sparqlService(endpoint, query);
         Model model = exec.execConstruct();
@@ -32,6 +32,45 @@ public class Request {
             FileWriter writer = new FileWriter("./request.txt");
             model.write(writer, "JSONLD");
             content = new String(Files.readAllBytes(Paths.get("./request.txt")));
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return content;
+    }
+
+    public static String getHospitalsByCity(String city) {
+        String content = "";
+        String endpoint = "http://localhost:3030/hospitals/sparql";
+        String request = "PREFIX rdf: <https://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+        + "PREFIX db: <http://dbpedia.org/ontology/> "
+        + "PREFIX ns: <https://www.w3.org/2006/vcard/ns#> "
+        + "PREFIX wd: <https://www.wikidata.org/wiki/> "
+        + "PREFIX mo: <http://purl.org/ontology/mo/> "
+        + "CONSTRUCT { ?hospital db:name ?name ; "
+        + "db:bedCount ?bed ; "
+        + "db:picture ?pic ; "
+        + "db:Website ?web ; "
+        + "db:address ?street ; "
+        + "ns:latitude ?lat ; "
+        + "ns:longitude ?lon ; "
+        + "mo:wikipedia ?wiki . }"
+        + "WHERE { ?hospital rdf:type wd:Q16917 ; "
+        + "db:city '" + city + "' ; "
+        + "db:name ?name ; "
+        + "db:bedCount ?bed ; "
+        + "db:picture ?pic ; "
+        + "db:Website ?web ; "
+        + "db:address ?street ; "
+        + "ns:latitude ?lat ; "
+        + "ns:longitude ?lon ; "
+        + "mo:wikipedia ?wiki . }";
+        Query query = QueryFactory.create(request, Syntax.syntaxARQ);
+        QueryExecution exec = QueryExecutionFactory.sparqlService(endpoint, query);
+        Model model = exec.execConstruct();
+        try {
+            FileWriter writer = new FileWriter("./requestHospitals.txt");
+            model.write(writer, "JSONLD");
+            content = new String(Files.readAllBytes(Paths.get("./requestHospitals.txt")));
         } catch (IOException e) {
             System.err.println(e);
         }
